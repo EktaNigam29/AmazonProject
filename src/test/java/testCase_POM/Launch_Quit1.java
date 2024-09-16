53d31d110ba6c385d2ed576748fb78d7d1b1a671
Ekta <nigam.ekta29@gmail.com>
2024-09-16 09:06:25 +0530

    Amazon Project

---
+++ src/test/java/testCase_POM/Launch_Quit1.java
@@ -0,0 +1,36 @@
+package testCase_POM;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import utility_packages.Listen1;
+
+public class Launch_Quit1 extends Listen1{
+	
+	
+	@BeforeMethod
+  public void launch()
+	{
+	
+		driver =new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+				
+	}
+	
+	@AfterMethod()
+	public void post1()
+	{
+		driver.quit();
+	}
+
+	
+
+}
